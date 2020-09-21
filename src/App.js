1dd979e676471aca43f36ad80db3e76f38bf6923
Shakeel Haider <43218437+shkhaider2015@users.noreply.github.com>
2020-09-21 18:22:43 +0500

    ctx

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import './App.css';
-import Sameer from './Components/sameer'
-
+import Sameer from './Components/sameer';
+import Shakeel from './Components/Shakeel';
+import {FirstGlassProvider, MyGlassContext, MyGlassContextTwo, SecondGlassProvider} from "./Context/GlassContext";
 function App() {
-
  
+  const fCTX = useContext(MyGlassContext);
+  const sCTX = useContext(MyGlassContextTwo);
 
   return (
     <div className="App">
-    
+     <h1>First Context : {fCTX} </h1>
+     <h1>Second Context : {sCTX} </h1>
+
+    <FirstGlassProvider>
       <Sameer />
+      <Suleman />
+    </FirstGlassProvider>
+
+    <SecondGlassProvider >
+      <Shakeel />
+    </SecondGlassProvider>
+
     </div>
   );
 }
--- src/Context/GlassContext.js
+++ src/Context/GlassContext.js
@@ -3,5 +3,25 @@ import React, { createContext } from "react";
 const MyGlassContext = createContext(0);
 const MyGlassContextTwo = createContext(10);
 
-export default MyGlassContext;
-export default MyGlassContextTwo;
+export {MyGlassContext};
+export {MyGlassContextTwo};
+
+function FirstGlassProvider()
+{
+    return <MyGlassContext.Provider
+    value={0}
+    >
+    </MyGlassContext.Provider>
+
+}
+
+function SecondGlassProvider()
+{
+    return <MySecondGlassContext.Provider
+    value={10}
+    >
+    </MySecondGlassContext.Provider>
+}
+
+export {FirstGlassProvider};
+export {SecondGlassProvider};
